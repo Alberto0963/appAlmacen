4f132950c782d94d155ca78439192ac988a2c53b
Alberto <gallegos.h.alberto@gmail.com>
2022-05-19 19:24:20 -0500

    obtener mis proveedores

---
+++ app/models/my_suppliers_view.rb
@@ -0,0 +1,11 @@
+class MySuppliersView < ApplicationRecord
+    self.table_name = 'suppliersView'
+
+    # validates :idSupplier, presence: true
+
+    # validates :addrress, presence: true
+    # validates :email, presence: true
+    # validates :name, presence: true
+    # validates :phone, presence: true
+    # validates :created_at, presence: true
+end
