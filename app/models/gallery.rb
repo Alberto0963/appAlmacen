4bb866e1ea23b5d00ddf17b0c6ee4c16052e21db
Alberto Gallegos <gallegos.h.alberto@gmail.com>
2022-05-23 13:25:14 -0500

    solucion relaciones galerias produto

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -6,4 +6,6 @@ class Gallery < ApplicationRecord
     validates :image, presence: true
 
     belongs_to :supplierProductView
+    # belongs_to :ViewCarShop, :foreign_key => "idProduct"
+
 end
--- app/models/view_car_shop.rb
+++ app/models/view_car_shop.rb
@@ -2,6 +2,6 @@ class ViewCarShop < ApplicationRecord
     self.table_name = 'viewCarShop'
     self.primary_key = 'carshopID'
 
-    has_many :gallery, foreign_key: :idProduct
-
+    # has_many :gallery, foreign_key: :idProduct
+    has_many :gallery, primary_key: "idProduct", foreign_key: 'idProduct'
 end
